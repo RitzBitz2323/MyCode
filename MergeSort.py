ef5f1c7578a74423fa742b05352e05c460bf77e3
Ritvik Ambekar <ritvikemail@gmail.com>
2020-01-13 15:26:11 -0600

    Edited Merge Sort

---
+++ MergeSort.py
@@ -0,0 +1,11 @@
+def mergeSort(a, l, r):
+    if p > r:
+        return
+    mid = len(a)//2
+    mergeSort(a, l, mid)
+    mergeSort(a, mid+1, r)
+    merge(A, l, mid, r)
+
+def merge(a, l, mid, r):
+
+
