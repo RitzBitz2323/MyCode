0501c656586fa97184949fe1b140f1bceb690598
Ritvik Ambekar <ritvikemail@gmail.com>
2020-01-13 22:10:58 -0600

    Switched To Creating Non Recursive MergeSort

--- MergeSort.py
+++ MergeSort.py
@@ -1,12 +1,3 @@
-def mergeSort(a, l, r):
-    if p > r:
-        return
-    mid = len(a)//2
-    mergeSort(a, l, mid)
-    mergeSort(a, mid+1, r)
-    merge(A, l, mid, r)
-
-def merge(a, l, mid, r):
-    return
-
-
+def mergeSort(array):
+    middle = len(arr)//2
+    
