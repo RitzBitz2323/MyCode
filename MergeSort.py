0275d029e751ec6e0586b421fef63f9994ba32db
Ritvik Ambekar <ritvikemail@gmail.com>
2020-01-14 17:54:16 -0600

    Added Runner and PrintArray Code

--- MergeSort.py
+++ MergeSort.py
@@ -1,14 +1,14 @@
 def mergeSort(array):
     while(len(array)>1):
         middle = len(array)//2 # Finds middle of array
-        left = array[:mid]  # Left side of array gets split off
-        right= array[mid:]  # Right side of array gets split off
+        left = array[:middle]  # Left side of array gets split off
+        right= array[middle:]  # Right side of array gets split off
 
         #while(len(left) > 1 and len(right) > 1):     May use code like this or this exact to make it non recursive
 
-        a = 0
-        b = 0
-        c = 0
+        i = 0
+        j = 0
+        k = 0
 
         # Copies data to temporary arrays left [] and right [[]
         while i < len(left) and j < len(right):
@@ -20,5 +20,17 @@ def mergeSort(array):
                 j=j+1
             k=k+1
 
-    print(*array)
+def printList(array):
+    for i in range(len(array)):
+        print(array[i], end=" ")
+    print()
 
+
+
+print("Merge Sort")
+a = [5,4,1,2,3,8,6]
+print("Initial Array")
+printList(a)
+print("Sorted Array")
+mergeSort(a)
+printList(a)
