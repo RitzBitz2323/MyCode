370993b3b91efb90f329374c212f402cfdb3be90
Ritvik Ambekar <ritvikemail@gmail.com>
2020-01-20 13:30:00 -0600

    Finished 2 Num Merge Sort

--- MergeSort2Num.py
+++ MergeSort2Num.py
@@ -1,2 +1,19 @@
 def mergeSort(a):
-    return
+    mid = len(a)//2
+    l = a[:mid]
+    r = a[mid:]
+    i = 0
+    if(l[i] > r[i]):
+        num = r[i]
+        r[i] = l[i]
+        l[i] = num
+    a[i] = l[i]
+    a[i+1] = r[i]
+    print(*a)
+
+
+
+
+print("Merge Sort 2 Numbers")
+arr = [100,7]
+mergeSort(arr)
