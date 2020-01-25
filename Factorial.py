8d5f97d4815c9979359eb77c5bf1d2dbd3547f6c
Ritvik Ambekar <ritvikemail@gmail.com>
2020-01-25 15:48:39 -0600

    Recursive Factorial

--- Factorial.py
+++ Factorial.py
@@ -1,13 +1,16 @@
 def factorial(number):
-    count = 0
-    index = number
-    factorial = 1
-    while(count < 8 and index > 0):
-        factorial = factorial*index
-        index = index - 1
-        count = count + 1
-    print(factorial)
+    if number >= 1:
+        return number*factorial(number-1)
+    else:
+        return 1
+
+# #    index = number
+# #    factorial = 1
+#     while(index > 0):
+#         factorial = factorial*index
+#         index = index - 1
+#     print(factorial)
 
 print("Factorial Program")
 num = int(input("Input a number you want the factorial of: "))
-factorial(num)
+print(factorial(num))
