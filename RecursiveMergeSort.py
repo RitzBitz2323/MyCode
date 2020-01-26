8b11ff5796cf54671fe07a92542f2a767663560e
Ritvik Ambekar <ritvikemail@gmail.com>
2020-01-26 11:35:23 -0600

    Edited File

--- RecursiveMergeSort.py
+++ RecursiveMergeSort.py
@@ -1,5 +1,25 @@
-def mergeSort():
-    return
+def mergeSort(arr):
+    if len(arr) > 1:
+        midpoint = len(arr)//2
+        leftSide = arr[:midpoint]
+        rightSide = arr[midpoint:]
+
+        mergeSort(leftSide)
+        mergeSort(rightSide)
+
+        arrayCount = 0
+        lsCount = 0
+        rsCount = 0
+        while lsCount < len(leftSide) and rsCount < len(rightSide):
+            if leftSide[lsCount] < rightSide[rsCount]:
+                arr[arrayCount] = leftSide[lsCount]
+                lsCount = lsCount + 1
+            else:
+                arr[arrayCount] = rightSide[rsCount]
+                rsCount = rsCount + 1
+            arrayCount = arrayCount + 1
+
+
 
 print("Recursive Merge Sort")
 arr = [5,1,3,2,8,6,4,9]
