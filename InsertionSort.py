1f3fab30e475907f01ff49e1673c60e45aa358aa
Ritvik Ambekar <ritvikemail@gmail.com>
2020-02-10 19:16:22 -0600

    Updated

---
+++ InsertionSort.py
@@ -0,0 +1,11 @@
+def insertSort(array):
+    tempArray = array
+    
+
+print("Insertion Sort")
+array = [1,7,4,9,2,6]
+print("Initial Array")
+print(*array)
+insertSort(array)
+print("Sorted Array")
+print(*array)
