73be0ae60d3a899005201d43931f0bd3fbb9aee6
Ritvik Ambekar <ritvikemail@gmail.com>
2020-02-29 09:51:31 -0600

    Init & Updated File

---
+++ LinkedList.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    llist.head.next = second
+    second.next = third
