f38fe1f7e4575e572cff5ab37d3c8c28bc9bf217
Ritvik Ambekar <ritvikemail@gmail.com>
2020-03-14 12:24:07 -0500

    Finished Delete , Print and Worked On Insert

--- LinkedList/myLinkedList/myLinkedList.java
+++ LinkedList/myLinkedList/myLinkedList.java
@@ -1,11 +1,62 @@
 
-
 public class myLinkedList
 {
-   Node start;
-   myLinkedList(Node x){
-    start = x;
+    Node start;
+    myLinkedList(Node x){
+        start = x;
     }
-   
-   
+
+    public static void delete(myLinkedList y, int pos){
+        if(pos == 0){
+            pos = pos;
+        }
+        else{
+            pos = pos - 1;
+        }
+        Node temp = y.start;
+        for(int i = 0; i < pos - 1; i++){
+
+            temp = temp.next;
+        }
+        Node tempNext = temp.next.next;
+        temp.next = tempNext;
+
+    }
+
+    public static void printAll(myLinkedList x){
+        Node y = x.start;
+
+        while(y.next != null){
+            System.out.print(y.data + ", ");
+            y = y.next;
+        }
+        System.out.print(y.data);
+        System.out.println();
+
+    }
+
+    public static void insert(myLinkedList y, int pos, int data){
+        if(pos == 0){
+            pos = pos;
+        }
+        else{
+            pos = pos - 1;
+        }
+        Node temp = y.start;
+        //insertNum = insertNum + 1;
+        for(int i = 0; i < pos - 1; i++){
+
+            temp = temp.next;
+        }
+        Node inserted = new Node(data, temp.next);
+        temp.next = inserted;
+
+    }
+
+    public static void delHead(myLinkedList y){
+        int pos = 0;
+        Node temp = y.start;
+
+    }
+
 }
--- LinkedList/myLinkedList/runner.java
+++ LinkedList/myLinkedList/runner.java
@@ -1,47 +1,33 @@
 /**
-   Previous printAll code:
-   System.out.println(y.start.data);
-   System.out.println(y.start.next.data);
-   System.out.println(y.start.next.next.data);
-   System.out.println(y.start.next.next.next.data); prints null
-   
-   Node y = x.start;
-      while(y.next != null){
-          System.out.println(y.data);
-          y = y.next;
-  
-        
-        }
-      System.out.println(y.data);
-   **/
+Previous printAll code:
+System.out.println(y.start.data);
+System.out.println(y.start.next.data);
+System.out.println(y.start.next.next.data);
+System.out.println(y.start.next.next.next.data); prints null
+
+Node y = x.start;
+while(y.next != null){
+System.out.println(y.data);
+y = y.next;
+
+}
+System.out.println(y.data);
+ **/
 
 public class runner
 {   
-    public static void printAll(myLinkedList x){
-      Node y = x.start;
-      
-        
-      do{
-          
-          System.out.println(y.data);
-        
-          y = y.next;
-        
-        }while(y.next != null);
-      
-    
-    
-    }
+  
+
     public static void main(String [] args){
         Node node1 = new Node(5, null);
         myLinkedList coolList = new myLinkedList(node1);
-        Node node2 = new Node(12, null);
-        node1.next = node2;
-        Node node3 = new Node(14, null);
-        node2.next = node3;
-        Node node4 = new Node(26,null);
-        node3.next = node4;
-        printAll(coolList);
-    
+        coolList.insert(coolList, 1, 135);
+        coolList.insert(coolList, 2, 21);
+        coolList.insert(coolList, 3, 43);
+        coolList.insert(coolList, 4, 982);
+        coolList.insert(coolList, 5, 34);
+        coolList.printAll(coolList);
+        //coolList.delete(coolList, 2);
+        
     }
 }
