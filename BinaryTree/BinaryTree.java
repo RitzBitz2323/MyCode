42ab5a46073e2bfaaa1521daf0c22d036ec471ec
Ritvik Ambekar <ritvikemail@gmail.com>
2020-03-21 12:59:22 -0500

    Updated

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,17 +1,25 @@
 
-
 public class BinaryTree
 {   
     Node root;
+    Node left;
+    Node right;
+    
     BinaryTree(int data){
         root = new Node(data);
-    
+
     }
-    
-    public void insert(Node x){
-        Node temp;
+
+    public void insert(int data){
+        Node temp = new Node(data);
         
-    
-    
+        if(this.left == null){
+            this.left = temp;
+
+        }
+        else if(this.right == null){
+            this.right = temp;
+        }
+
     }
 }
--- BinaryTree/Runner.java
+++ BinaryTree/Runner.java
@@ -5,10 +5,8 @@ public class Runner
         BinaryTree myTree = new BinaryTree(5);
         myTree.root.left = new Node(3);
         myTree.root.right = new Node(7);
-        myTree.root.left.left = new Node(15);
-        myTree.root.left.right = new Node(23);
-        myTree.root.right.left = new Node(47);
-        myTree.root.right.right = new Node(109);
+        myTree.insert(50);
+        
         
         
     
